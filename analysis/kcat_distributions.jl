053c26947352f72caf801a842acf7ad345ad955a
Frames White <me@oxinabox.net>
2024-10-23 10:08:12 +0800

    basic stats

--- analysis/kcat_distributions.jl
+++ analysis/kcat_distributions.jl
@@ -1,4 +1,26 @@
 using DLkitty
 using DLkitty: reaction_rates_table
-
+using DataFrames
+using Statistics
+using Plots
 tbl = reaction_rates_table()
+kcats = filter(
+    (
+        "parameter.type" => ==("kcat"),
+        "parameter.unit" => =="s^(-1)",
+    )
+    tbl)
+
+println("total kcats samples: ", nrow(kcats))
+
+reaction_kcats = groupby(kcats, "Reaction")
+reaction_kcat_counts = combine(nrow, reaction_kcats).nrow
+println("total reactions: ", length(reaction_kcat_counts))
+histogram(reaction_kcat_counts, xlabel="number of kcat samples")
+
+
+_, most_common_reaction = findmax(nrow, reaction_kcats)
+most_common_reaction_tbl = reaction_kcats[most_common_reaction]
+vscodedisplay(most_common_reaction_tbl)
+
+histogram(most_common_reaction_tbl.var"parameter.startValue")
