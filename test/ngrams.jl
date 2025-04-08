1066c5e33eeeb9ea1a2f8dc47022a898e9f291b8
augustinas1 <augustinas.sukys@gmail.com>
2025-04-08 14:52:55 +1000

    fix ngrams test

--- test/ngrams.jl
+++ test/ngrams.jl
@@ -2,9 +2,8 @@
     @test DLkitty.ngrams("ABCD", 3) == ["-AB", "ABC", "BCD", "CD="]
 end
 
-
 @testset "load" begin
-    ngrams = DLkitty.load_all_sequence_ngrams(3)
+    ngrams = DLkitty.all_sequence_ngrams(kcat_table_train(), 3)
     @test length(ngrams) > 8_000
     @test all(x==DLkitty.UNKNOWN_NGRAM || length(x)==3 for x in ngrams)
 end
